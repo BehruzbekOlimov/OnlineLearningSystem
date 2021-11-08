d6eb105fd6da072a8ae1dccdca5c7b3043b37df1
Behruzbek <olimovbehruzbek@gmail.com>
2021-11-08 09:33:48 +0500

    Initial commit

---
+++ src/test/java/uz/tuit/onlinelearningsystem/OnlineLearningSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.tuit.onlinelearningsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineLearningSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
